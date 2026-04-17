cbf78192bab3059ebffc7db8a07eb2f7d1311f4c
daoyu <daoyu1024@icloud.com>
2026-04-17 12:59:17 +0800

    fixed a async bug blocking UI

--- HookForm/TransferProxy.cs
+++ HookForm/TransferProxy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AgeHood.HookTest
@@ -18,8 +20,8 @@ namespace AgeHood.HookTest
         private readonly TcpClient _tcpClient = new TcpClient();
         private readonly UdpClient _udpProxy = new UdpClient(new IPEndPoint(IPAddress.Loopback, 0));
         private NetworkStream _stream;
-        private BinaryReader _br;
-        private BinaryWriter _bw;
+        // writer semaphore to serialize writes to the network stream
+        private readonly SemaphoreSlim _writeSemaphore = new SemaphoreSlim(1, 1);
         private readonly ushort _udpProxyPort;
         private readonly ushort _tcpProxyPort;
         private uint _virtualIp;
@@ -57,15 +59,28 @@ namespace AgeHood.HookTest
             _state = ProxyState.Starting;
             try
             {
+                // Connect without blocking caller
                 await _tcpClient.ConnectAsync(_remoteHost, _remotePort);
                 _stream = _tcpClient.GetStream();
-                _br = new BinaryReader(_stream);
-                _bw = new BinaryWriter(_stream);
-                _virtualIp = _br.ReadUInt32();
+
+                // Read 4 bytes virtual IP from stream asynchronously
+                var vIpBuf = new byte[4];
+                var read = 0;
+                while (read < 4)
+                {
+                    var n = await _stream.ReadAsync(vIpBuf, read, 4 - read);
+                    if (n == 0) throw new EndOfStreamException("Remote closed connection while reading virtual IP");
+                    read += n;
+                }
+                // little-endian
+                _virtualIp = (uint)(vIpBuf[0] | (vIpBuf[1] << 8) | (vIpBuf[2] << 16) | (vIpBuf[3] << 24));
+
                 _tcpListener.Start();
-                UdpProxyLoop();
-                StreamLoop();
-                TcpProxyLoop();
+
+                _ = UdpProxyLoop();
+                _ = StreamLoop();
+                _ = TcpProxyLoop();
+
                 _state = ProxyState.Running;
             }
             catch
@@ -78,26 +93,15 @@ namespace AgeHood.HookTest
         public void Close()
         {
             _state = ProxyState.Stopped;
-            if (_bw != null)
-            {
-                _bw.Close();
-            }
-            if (_br != null)
-            {
-                _br.Close();
-            }
-            if (_stream != null)
-            {
-                _stream.Close();
-            }
+            // ensure stream closed
+            _stream?.Close();
             if (_tcpClient != null && _tcpClient.Connected)
             {
                 _tcpClient.Close();
             }
-            if (_udpProxy != null)
-            {
-                _udpProxy.Close();
-            }
+
+            _udpProxy?.Close();
+
             if (_tcpListener != null)
             {
                 try
@@ -117,16 +121,20 @@ namespace AgeHood.HookTest
         {
             while (true)
             {
-                var packet = (await _udpProxy.ReceiveAsync()).Buffer;
-                using (var ms = new MemoryStream(packet))
-                using (var br = new BinaryReader(ms))
+                var result = await _udpProxy.ReceiveAsync();
+                var packet = result.Buffer;
+                // Forward raw packet to remote over TCP stream
+                if (_stream != null)
                 {
-                    var command = br.ReadByte();
-                    switch (command)
+                    await _writeSemaphore.WaitAsync();
+                    try
                     {
-                        case 1://broadcast
-                            _bw.Write(packet);
-                            break;
+                        await _stream.WriteAsync(packet, 0, packet.Length);
+                        await _stream.FlushAsync();
+                    }
+                    finally
+                    {
+                        _writeSemaphore.Release();
                     }
                 }
             }
@@ -134,31 +142,42 @@ namespace AgeHood.HookTest
 
         private async Task StreamLoop()
         {
+            var header = new byte[17];
             while (true)
             {
-                var command = _br.ReadByte();
-                var fromVip = _br.ReadUInt32();
-                var fromPort = _br.ReadUInt16();
-                var toVip = _br.ReadUInt32();
-                var toPort = _br.ReadUInt16();
-                var length = _br.ReadInt32();
-                var data = _br.ReadBytes(length);
-                var packet = new byte[length + 17];
-                using (var ms = new MemoryStream(packet))
-                using (var bw = new BinaryWriter(ms))
+                // read exactly 17 bytes header
+                var read = 0;
+                while (read < 17)
                 {
-                    bw.Write(command);
-                    bw.Write(fromVip);
-                    bw.Write(fromPort);
-                    bw.Write(toVip);
-                    bw.Write(toPort);
-                    bw.Write(length);
-                    bw.Write(data);
+                    var n = await _stream.ReadAsync(header, read, 17 - read);
+                    if (n == 0) throw new EndOfStreamException("Remote closed connection");
+                    read += n;
                 }
+
+                var command = header[0];
+                var fromVip = (uint)(header[1] | (header[2] << 8) | (header[3] << 16) | (header[4] << 24));
+                var fromPort = (ushort)(header[5] | (header[6] << 8));
+                var toVip = (uint)(header[7] | (header[8] << 8) | (header[9] << 16) | (header[10] << 24));
+                var toPort = (ushort)(header[11] | (header[12] << 8));
+                var length = header[13] | (header[14] << 8) | (header[15] << 16) | (header[16] << 24);
+
+                var data = new byte[length];
+                read = 0;
+                while (read < length)
+                {
+                    var n = await _stream.ReadAsync(data, read, length - read);
+                    if (n == 0) throw new EndOfStreamException("Remote closed connection while reading payload");
+                    read += n;
+                }
+
+                var packet = new byte[length + 17];
+                Buffer.BlockCopy(header, 0, packet, 0, 17);
+                Buffer.BlockCopy(data, 0, packet, 17, length);
+
                 switch (command)
                 {
                     case 1://udp sendto
-                        _udpProxy.SendAsync(packet, packet.Length, new IPEndPoint(IPAddress.Loopback, toPort));
+                        await _udpProxy.SendAsync(packet, packet.Length, new IPEndPoint(IPAddress.Loopback, toPort));
                         break;
                 }
             }
